11e6f8461546f481f349e1e5eafe1389e6b88049
Lucas B <40246581+Lucbh@users.noreply.github.com>
2018-07-07 11:53:08 -0700

    SUm

--- src/BookOfIllusions.java
+++ src/BookOfIllusions.java
@@ -10,12 +10,13 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
 
 public class BookOfIllusions extends MouseAdapter {
-
+	
 	/*
 	 * Here we are instantiating our BookOfIllusions class and calling it’s createBook() method. This is because we want to get out of the
 	 * static main method, so that we can add a click listener to each illusion.
@@ -27,24 +28,44 @@ public class BookOfIllusions extends MouseAdapter {
 	}
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+	
+	JFrame j = new JFrame();
+	JLabel l;
+	
 	private void createBook() {
 		// 2. make the frame visible
+		j.setVisible(true);
 		// 3. set the size of the frame
+		j.setSize(500, 500);
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+			String picName1 = "scottish.jpg";
+		
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+			l = loadImageFromComputer(picName1);
 		// 8. add your JLabel to the frame
+			j.add(l);
 		// 9. call the pack() method on the frame
+			j.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		j.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("clicked");
 		// 12. remove everything from the frame that was added earlier
+		j.remove(l);
 		// 13. load a new image like before (this is more than one line of code)
+		String picName2 = "woods.jpg";
+		JLabel jl;
+		jl = loadImageFromComputer(picName2);
+		j.add(jl);
+		
 		// 14. pack the frame
+		j.pack();
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
